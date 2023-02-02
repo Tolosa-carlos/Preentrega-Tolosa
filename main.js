5173d5538427158446cc3bc6f7e167861c7698cf
Carlos Tolosa <carloswtolosa@gmail.com>
2023-02-01 21:03:25 -0300

    First commit

---
+++ main.js
@@ -0,0 +1,28 @@
+
+
+let valor = parseInt(prompt("Ingrese un monto a cotizar el cambio: "))
+let resultado = 0;
+let dolarBlue = 187;
+let real = 37;
+let euro = 206;
+let opcion = parseInt(prompt("Eliga el tipo de cambio: 1. Dolar 2. Real 3. Euro")) 
+
+
+
+if(opcion === 1){
+    resultado = valor / dolarBlue;
+    alert("El cambio de pesos a dolares es: $ " + resultado)
+}
+else if(opcion === 2){
+    resultado = valor / real;
+    alert("El cambio de pesos a reales es: $ " + resultado)
+}
+else if(opcion === 3){
+    resultado = valor / euro;
+    alert("El cambio de pesos a euros es: $ " + resultado)
+}
+else{
+    alert("Eliga una de las opciones disponibles")
+}
+    
+
