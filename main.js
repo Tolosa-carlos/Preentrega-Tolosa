86ea89ee5c1b85026e86aa85a123d48698288b15
Carlos Tolosa <carloswtolosa@gmail.com>
2023-02-07 14:45:14 -0300

    Funcion

--- main.js
+++ main.js
@@ -8,21 +8,28 @@ let euro = 206;
 let opcion = parseInt(prompt("Eliga el tipo de cambio: 1. Dolar 2. Real 3. Euro")) 
 
 
+function cotizar(){
 
-if(opcion === 1){
+
+    if(opcion === 1){
     resultado = valor / dolarBlue;
     alert("El cambio de pesos a dolares es: $ " + resultado)
-}
-else if(opcion === 2){
+    }
+    else if(opcion === 2){
     resultado = valor / real;
     alert("El cambio de pesos a reales es: $ " + resultado)
-}
-else if(opcion === 3){
+    }
+    else if(opcion === 3){
     resultado = valor / euro;
     alert("El cambio de pesos a euros es: $ " + resultado)
+    }
+
+
 }
-else{
-    alert("Eliga una de las opciones disponibles")
-}
-    
+
+
+
+
+cotizar();
+alert("Listo")
 
