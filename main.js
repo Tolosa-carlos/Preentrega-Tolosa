74d8ce2e4d983281d4c1b3495aa96ca3099e120a
Carlos Tolosa <carloswtolosa@gmail.com>
2023-02-07 15:34:36 -0300

    Ciclo agregado

--- main.js
+++ main.js
@@ -1,27 +1,35 @@
 
 
-let valor = parseInt(prompt("Ingrese un monto a cotizar el cambio: "))
 let resultado = 0;
-let dolarBlue = 187;
+let dolarBlue = 189;
 let real = 37;
-let euro = 206;
+let euro = 203;
 let opcion = parseInt(prompt("Eliga el tipo de cambio: 1. Dolar 2. Real 3. Euro")) 
 
 
+
+while( opcion != 1 && opcion != 2 && opcion != 3 ){
+
+    alert("Por favor, eliga alguna de las siguientes opciones")
+    opcion = parseInt(prompt("Eliga el tipo de cambio: 1. Dolar 2. Real 3. Euro"));
+
+}
+
 function cotizar(){
 
+    let valor = parseInt(prompt("Ingrese un monto a cotizar el cambio: "))
 
-    if(opcion === 1){
+    if(opcion == 1){
     resultado = valor / dolarBlue;
     alert("El cambio de pesos a dolares es: $ " + resultado)
     }
-    else if(opcion === 2){
+    else if(opcion == 2){
     resultado = valor / real;
     alert("El cambio de pesos a reales es: $ " + resultado)
     }
-    else if(opcion === 3){
+    else if(opcion == 3){
     resultado = valor / euro;
-    alert("El cambio de pesos a euros es: $ " + resultado)
+   alert("El cambio de pesos a euros es: € " + resultado)
     }
 
 
@@ -29,7 +37,5 @@ function cotizar(){
 
 
 
-
 cotizar();
-alert("Listo")
 
