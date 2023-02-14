1fa0e8346427afe2729e9478f3a206236f3fcc01
Carlos Tolosa <carloswtolosa@gmail.com>
2023-02-14 18:30:34 -0300

    Metodo de busqueda

--- main.js
+++ main.js
@@ -1,14 +1,26 @@
 
+alert("Bienvenido")
 
-let opcion = parseInt(prompt("Eliga el tipo de cambio: 1. Dolar 2. Real 3. Euro 4. Libra")) 
-
-const moneda = [
-    { nombre: "dolares", precio: 190, sign: "$" },
-    { nombre: "realeas", precio: 37, sign: "R$"},
-    { nombre: "euros", precio: 203, sign: "€"},
-    { nombre: "libras", precio: 229, sign: "£"}
+const monedas = [
+    { nombre: "dolar", precio: 190, sign: "$" },
+    { nombre: "real", precio: 37, sign: "R$"},
+    { nombre: "euro", precio: 203, sign: "€"},
+    { nombre: "libra", precio: 229, sign: "£"}
 ]
 
+
+let nombre = prompt("Ingrese la moneda a cotizar: ")
+let find = monedas.find(moneda => moneda.nombre === nombre)
+let mensaje = `
+
+    Moneda: ${find.nombre}
+    Precio: ${find.sign} ${find.precio}
+
+`
+alert(mensaje)
+
+
+let opcion = parseInt(prompt("Eliga el tipo de cambio: 1. Dolar 2. Real 3. Euro 4. Libra")) 
 
 while( opcion != 1 && opcion != 2 && opcion != 3 && opcion != 4){
 
@@ -23,23 +35,23 @@ function cotizar(){
     let valor = parseInt(prompt("Ingrese un monto a cotizar: "))
 
 
-    for ( let i = 0; i < moneda.length; i++)
+    for ( let i = 0; i < monedas.length; i++)
 
         if(opcion == 1 && i == 0){
-        resultado = valor / moneda[i].precio;
-        alert(`El cambio de pesos a ${moneda[i].nombre} es: ${moneda[i].sign}  ${resultado}`)    
+        resultado = valor / monedas[i].precio;
+        alert(`El cambio de pesos a ${monedas[i].nombre} es: ${monedas[i].sign}  ${resultado}`)    
         }
         else if(opcion == 2 && i == 1){
-        resultado = valor / moneda[i].precio;
-        alert(`El cambio de pesos a ${moneda[i].nombre} es: ${moneda[i].sign}  ${resultado}`)
+        resultado = valor / monedas[i].precio;
+        alert(`El cambio de pesos a ${monedas[i].nombre} es: ${monedas[i].sign}  ${resultado}`)
         }
         else if(opcion == 3 && i == 2){
-        resultado = valor / moneda[i].precio;
-        alert(`El cambio de pesos a ${moneda[i].nombre} es: ${moneda[i].sign}  ${resultado}`)
+        resultado = valor / monedas[i].precio;
+        alert(`El cambio de pesos a ${monedas[i].nombre} es: ${monedas[i].sign}  ${resultado}`)
         }
         else if(opcion == 4 && i == 3){
-        resultado = valor / moneda[i].precio;
-        alert(`El cambio de pesos a ${moneda[i].nombre} es: ${moneda[i].sign}  ${resultado}`)
+        resultado = valor / monedas[i].precio;
+        alert(`El cambio de pesos a ${monedas[i].nombre} es: ${monedas[i].sign}  ${resultado}`)
         }
 
 
